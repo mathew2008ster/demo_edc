6128d02be00aac22fc45b372a6ac7825156fa94a
gengyu <476413818@qq.com>
2018-06-01 18:25:47 +0800

    小修改

--- public/js/common.js
+++ public/js/common.js
@@ -1,21 +1,23 @@
 /**
  * Created by admin on 2018/6/1.
  */
-$('#sidebar2').insertBefore('.page-content');
-$('#navbar').addClass('h-navbar');
-$('.footer').insertAfter('.page-content');
+$(function(){
+    $('#sidebar2').insertBefore('.page-content');
+    $('#navbar').addClass('h-navbar');
+    $('.footer').insertAfter('.page-content');
 
-$('.page-content').addClass('main-content');
+    $('.page-content').addClass('main-content');
 
-$('.menu-toggler[data-target="#sidebar2"]').insertBefore('.navbar-brand');
+    $('.menu-toggler[data-target="#sidebar2"]').insertBefore('.navbar-brand');
 
 
-$(document).on('settings.ace.two_menu', function(e, event_name, event_val) {
-    if(event_name == 'sidebar_fixed') {
-        if( $('#sidebar').hasClass('sidebar-fixed') ) $('#sidebar2').addClass('sidebar-fixed')
-        else $('#sidebar2').removeClass('sidebar-fixed')
-    }
-}).triggerHandler('settings.ace.two_menu', ['sidebar_fixed' ,$('#sidebar').hasClass('sidebar-fixed')]);
+    $(document).on('settings.ace.two_menu', function(e, event_name, event_val) {
+        if(event_name == 'sidebar_fixed') {
+            if( $('#sidebar').hasClass('sidebar-fixed') ) $('#sidebar2').addClass('sidebar-fixed')
+            else $('#sidebar2').removeClass('sidebar-fixed')
+        }
+    }).triggerHandler('settings.ace.two_menu', ['sidebar_fixed' ,$('#sidebar').hasClass('sidebar-fixed')]);
 
-$('#sidebar2[data-sidebar-hover=true]').ace_sidebar_hover('reset');
-$('#sidebar2[data-sidebar-scroll=true]').ace_sidebar_scroll('reset', true);
+    $('#sidebar2[data-sidebar-hover=true]').ace_sidebar_hover('reset');
+    $('#sidebar2[data-sidebar-scroll=true]').ace_sidebar_scroll('reset', true);
+});
